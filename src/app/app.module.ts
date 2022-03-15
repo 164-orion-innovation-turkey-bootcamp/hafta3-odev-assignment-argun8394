ae3ce1aacdd89e0e9e1a6d67c3f99d6fd6da5105
kodluyoruz <info@kodluyoruz.org>
2022-03-15 23:20:06 +0300

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroupComponent } from './form/form-group/form-group.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormGroupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form/form-group/form-group.component.ts
@@ -0,0 +1,47 @@
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'form-group',
+  templateUrl: './form-group.component.html',
+  styleUrls: ['./form-group.component.css']
+})
+export class FormGroupComponent  {
+
+  //Tüm inputlara etkileşim sağlanmıştır
+  productForm = new FormGroup({
+    name : new FormControl("",
+    [
+      Validators.required,
+
+  ]),
+    description : new FormControl("",Validators.minLength(20)),
+    price : new FormControl("",Validators.required),
+    model : new FormControl("",Validators.required),
+
+
+
+  })
+
+  get name() {
+    return this.productForm.get("name")
+  }
+
+  get description() {
+    return this.productForm.get("description")
+  }
+
+  onSubmit (){
+   console.log(this.productForm.value)//burda product form nesnesinin value larını yazdırmış olduk
+  }
+
+update() {
+  this.productForm.patchValue({//Burada patchValue() metodu ile sadece istediğimiz değerleri değiştiririz
+    //setValue() metodu kullanacak olursak tüm formControl inputlarını değiştirmeye çalışır
+    //name : "Iphone X",
+    //price : "7500"
+  })
+}
+
+}
